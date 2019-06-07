f35f3b232ccb45a5b5cab7cfcdab09d625ae5646
MatWilk1 <mat.wilk@gmail.com>
2019-06-07 20:29:36 +0200

    poprawki

--- src/tdd/bankaccount/AccountTest.java
+++ src/tdd/bankaccount/AccountTest.java
@@ -12,14 +12,15 @@ import org.junit.jupiter.api.Test;
 
 public class AccountTest {
 	
-//	@BeforeEach
-//	public void setUp() throws Exception {
-//		Account a = new Account("Adam", 100);
-//	}
+	private Account a;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		a = new Account("Adam", 100);
+	}
 	
 	@Test
 	public void depositMoneyTest() {
-		Account a = new Account("Adam", 100);
 		int expected = 150;
 		int result = a.depositMoney(50);
 		
@@ -28,7 +29,6 @@ public class AccountTest {
 	
 	@Test
 	public void takeMoneyTest() throws Exception {
-		Account a = new Account("Adam", 100);
 		int expected = 50;
 		int result = a.takeMoney(50);
 		
@@ -37,14 +37,11 @@ public class AccountTest {
 	
 	@Test
 	public void takeMoneyToMuchTest() {
-		Account a = new Account("Adam", 100);
-		
 		assertThrows(Exception.class, () -> a.takeMoney(300));
 	}
 	
 	@Test
 	public void setTakeLimitTest() {
-		Account a = new Account("Adam", 100);
 		a.setTakeLimit(70);
 		
 		assertThrows(Exception.class, () -> a.takeMoney(80));
@@ -52,8 +49,6 @@ public class AccountTest {
 	
 	@Test
 	public void createInvestmentTest() throws Exception {
-		Account a = new Account("Adam", 100);
-		
 		Investment result = a.createInvestment(40, 12);
 		
 		assertEquals(60, a.getBalance());
@@ -64,8 +59,6 @@ public class AccountTest {
 	
 	@Test
 	public void createInvestmentToMuchTest() throws Exception {
-		Account a = new Account("Adam", 100);
-		
 		assertThrows(Exception.class, () -> a.createInvestment(150, 12));
 	}
 	
